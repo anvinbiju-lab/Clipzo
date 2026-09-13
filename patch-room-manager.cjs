111e362a34e3b194df5e177786fdae3109bce824
Anvin Biju <anvinbiju1@gmail.com>
2026-09-13 22:17:44 +0530

    chore: remove patch scripts

--- patch-room-manager.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server/room-manager.ts', 'utf8');
-content = content.replace(
-  /seenMessageIds: Set<string>;/,
-  `seenMessageIds: Set<string>;
-  httpMessageQueue?: Array<{ t: 'msg', id: string, d: string, ts: number }>;`
-);
-fs.writeFileSync('server/room-manager.ts', content);
-console.log('RoomManager patched.');
